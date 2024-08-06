33eee5d08c7aee812229b1bf3ece245429fc9312
srmu <geo>
2024-08-06 09:06:33 +0530

    new setup in lab

---
+++ !pass.c
@@ -0,0 +1,24 @@
+/*Write a program to determine whether a student has passed or failed. To pass, a
+student requires a total of 40% and at least 33% in each subject. Assume there
+are three subjects and take the marks as input from the user.*/
+
+
+
+
+#include<stdio.h>
+int main(){
+    int mark1, mark2, mark3;
+    float total, percentage;
+    int pass=1;
+
+    // Taking input from the user
+
+    printf("Enter mark for subject:\n");
+    scanf("f", &mark1);
+    printf("Enter mark for subject:\n");
+    scanf("f", &mark2);
+    printf("Enter mark for subject:\n");
+    scanf("f", &mark3);
+
+
+}
