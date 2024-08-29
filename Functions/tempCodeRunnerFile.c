1a1b4da91f9a7d66f236d66d7125f815511d95c4
srmu <geo>
2024-08-29 10:25:52 +0530

    long time

--- Functions/tempCodeRunnerFile.c
+++ Functions/tempCodeRunnerFile.c
@@ -1,1 +1,11 @@
-return 0;
+#include<stdio.h>
+int main(){
+    int a ,b;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    printf("enter the value of b");
+    scanf("%d",b);
+    printf("your output is %d ",a+b/2);
+    return 0;
+
+}
---
+++ Pointers/address_of_var.c
@@ -0,0 +1,14 @@
+/* Write a program to print the address of a variable. Use this address to get the
+value of the variable. */
+
+#include <stdio.h>
+
+int main() {
+    int a=20;
+    int *j;
+    j=&a;
+
+    printf(&);
+    
+    return 0;
+}
